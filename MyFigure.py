95c607fdd63b106ffa1691fa2da92dcfb720c478
lovetea10 <lulushkina.alina@gmail.com>
2024-04-18 00:44:11 +0300

    18.04

---
+++ MyFigure.py
@@ -0,0 +1,24 @@
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+class MyFigure(FigureCanvas):
+    def __init__(self,width=5, height=4, dpi=100):
+
+        self.fig = Figure(figsize=(width, height), dpi=dpi)
+        super(MyFigure,self).__init__(self.fig)
+        self.axes = self.fig.add_subplot(111)
+
+class MyFigurePolar(FigureCanvas):
+
+    def __init__(self,width=5, height=4, dpi=100):
+
+        self.fig = Figure(figsize=(width, height), dpi=dpi)
+        super(MyFigurePolar,self).__init__(self.fig)
+        self.axes = self.fig.add_subplot(111, projection = 'polar')
+
+class MyFigure3d(FigureCanvas):
+
+    def __init__(self,width=5, height=4, dpi=100):
+
+        self.fig = Figure(figsize=(width, height), dpi=dpi)
+        super(MyFigure3d,self).__init__(self.fig)
+        self.axes = self.fig.add_subplot(111, projection='3d')
